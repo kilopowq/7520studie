02097b39a9f4ce2190a687a6381f42484621dd28
Pasha7520 <pasha.ganuliak@mail.ru>
2018-05-11 20:50:23 +0300

    First Change

--- src/main/java/com/kilopo/kosshop/Main.java
+++ src/main/java/com/kilopo/kosshop/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Oki doki!!!");
         System.out.println("I've already been there!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!");
     }
 }
