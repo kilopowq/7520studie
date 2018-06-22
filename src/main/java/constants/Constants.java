aa6d45ad7dafa94e7c8c7eaab9e6a6ca1c5db483
Pasha7520 <pasha.ganuliak@mail.ru>
2018-06-22 12:56:31 +0300

    KOS-22 add constants

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public interface Constants {
+    final String KOSSHOP = "com.kilopo.kosshop";
+
+    interface Database{
+        final String PROPERTIES = "classpath:db.properties";
+    }
+}
