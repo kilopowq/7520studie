7869b740554fa6e89b86c6d077bfbf0845bdfd5b
Pasha7520 <pasha.ganuliak@mail.ru>
2018-09-05 22:14:03 +0300

    KOS-24 add Apache tiles

--- src/main/java/com/kilopo/kosshop/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/kilopo/kosshop/service/Impl/CategoryServiceImpl.java
@@ -1,8 +1,8 @@
 package com.kilopo.kosshop.service.Impl;
 
+import com.kilopo.kosshop.service.CategoryService;
 import com.kilopo.kosshop.DAO.CategoryDAO;
 import com.kilopo.kosshop.entity.Category;
-import com.kilopo.kosshop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +12,6 @@ public class CategoryServiceImpl implements CategoryService {
     private CategoryDAO categoryDAO;
 
     public boolean addCategory(Category category) {
-        if (categoryDAO.addOrUpdate(category).getId() != null) {
-            return true;
-        } else {
-            return false;
-        }
+        return categoryDAO.addOrUpdate(category).getId() != null;
     }
 }
